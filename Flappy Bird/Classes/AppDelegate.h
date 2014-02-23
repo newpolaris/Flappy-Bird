544d2121a80ef104aa5e1cef45cdafa9524075a3
newpolaris <newpolaris@gmail.com>
2014-02-23 15:58:39 +0900

    Initial Commit

---
+++ Flappy Bird/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Flappy Bird
+//
+//  Created by newpolaris on 2/23/14.
+//  Copyright newpolaris 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
